adcbbdeda68677304570e40b904dc2524862f001
Eddyy1410 <eddynguyen14@gmail.com>
2025-12-14 23:13:31 +0700

    Chap 11: Mutating Data (Update and Delete an invoice) - Dynamic Route Segments - pass id to the Server Action using JS bind (ensure any values passed   to the Server Action are encoded)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,13 +17,10 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true})
 
 export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get("customerId"),
-    amount: formData.get("amount"),
-    status: formData.get("status"),
-  });
+  const { customerId, amount, status } = CreateInvoice.parse(Object.fromEntries(formData.entries()));
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
   // const rawFormData = Object.fromEntries(formData.entries());
@@ -38,4 +35,28 @@ export async function createInvoice(formData: FormData) {
   // Nếu không có bước này, trang sẽ vẫn hiển thị dữ liệu cũ đã được cache.
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
